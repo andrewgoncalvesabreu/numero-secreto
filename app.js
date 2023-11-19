a73fc4f6dc27aa45c3cefd3d6f7d35961be34835
Andrew Gonçalves Abreu <124524087+andrewgoncalvesabreu@users.noreply.github.com>
2023-11-19 18:01:43 -0300

    Deixando mais fácil

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Insira um número de 1 a 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
